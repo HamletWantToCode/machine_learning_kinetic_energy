501408eb208c00be1dd716079bd4206c02951b7d
hongbin <hongbinrenscu@outlook.com>
2018-11-26 19:26:17 +0800

    testing

--- tools/database.py
+++ tools/database.py
@@ -6,7 +6,7 @@ from MLEK.main.utils import potential_gen
 from MLEK.main.solver import solver
 
 LOW_NUM_Q = 1
-HIGH_NUM_Q = 10
+HIGH_NUM_Q = 30
 LOW_V0 = 0          # absolute value
 HIGH_V0 = 50
 LOW_dMU = 5
@@ -14,9 +14,9 @@ HIGH_dMU = 50
 RANDOM_STATE = 5
 
 param_gen = potential_gen(LOW_NUM_Q, HIGH_NUM_Q, LOW_V0, HIGH_V0, LOW_dMU, HIGH_dMU, RANDOM_STATE)
-data = Compute(4, 100, '../data_file/quantum_data', '../data_file/potential_data')
+data = Compute(1, 100, '../data_file/quantum_data', '../data_file/potential_data')
 data.add_solver(solver)
 
-nk = 40
-nbasis = 30
+nk = 100
+nbasis = 100
 data.run(nk, nbasis, param_gen)
