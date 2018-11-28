72e538ae05345ee01830c56fa285c7b139b30cd6
hongbinren <hongbinrenscu@outlook.com>
2018-11-28 09:52:51 +0800

    fixed electron number

--- main/solver.py
+++ main/solver.py
@@ -3,7 +3,7 @@
 import numpy as np 
 from scipy.linalg import eigh
 
-def solver(nk, nbasis, dmu, hamiton_mat):
+def solver(nk, nbasis, ne, hamiton_mat):
     kpoints = np.linspace(0, np.pi, nk)
     # build and solve eigenvalue problem
     T = 0
@@ -12,14 +12,12 @@ def solver(nk, nbasis, dmu, hamiton_mat):
         kinetic_term = [0.5*(k+(i-nbasis//2)*2*np.pi)**2 for i in range(nbasis)]
         np.fill_diagonal(hamiton_mat, kinetic_term)
         En_k, Uq_k = eigh(hamiton_mat, overwrite_a=True, overwrite_b=True)
-        if k == 0:
-            mu = En_k[0] + dmu
 
         # compute electron density
         # compute kinetic energy
         num_mat_eigspace = np.zeros((nbasis, nbasis))
         for i in range(nbasis):
-            if En_k[i] > mu:
+            if i+1 > ne:
                 break
             num_mat_eigspace[i, i] = 1
         density_mat_kspace = Uq_k @ (num_mat_eigspace @ (Uq_k.T).conj())
@@ -31,6 +29,7 @@ def solver(nk, nbasis, dmu, hamiton_mat):
             T_k += 0.5*((k+(i-nbasis//2)*2*np.pi)**2)*(density_mat_kspace[i, i]).real
         T += T_k
         density += density_k
+    mu = (En_k[ne-1] + En_k[ne])/2
     return T/nk, mu, density/nk, 
 
 
