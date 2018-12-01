67b77958923a8056ad38da2f38d373eb071afe2e
hongbin <hongbinrenscu@outlook.com>
2018-12-01 09:20:07 +0800

    local commit

--- tools/database.py
+++ tools/database.py
@@ -6,11 +6,12 @@ import pickle
 from MLEK.main.utils import potential_gen
 from MLEK.main.solver import solver
 
-NSAMPLES = 500
-LOW_NUM_Q = 1
-HIGH_NUM_Q = 20
-LOW_V0 = 0          # absolute value
-HIGH_V0 = 10
+NSAMPLES = 100
+# LOW_NUM_Q = 5
+# HIGH_NUM_Q = 8
+NQ = 2
+LOW_V0 = 5          # absolute value
+HIGH_V0 = 30
 NE = 1
 NK = 100
 NBASIS = 50
@@ -20,21 +21,22 @@ SIZE = comm.Get_size()
 NSAMPLE_PER_PROC = NSAMPLES // SIZE
 ID = comm.Get_rank()
 
-POTENTIAL_STORAGE = np.zeros((NSAMPLE_PER_PROC, NBASIS+1), dtype=np.complex64)
+POTENTIAL_STORAGE = np.zeros((NSAMPLE_PER_PROC, NBASIS), dtype=np.complex64)
 DATA_STORAGE = np.zeros((NSAMPLE_PER_PROC, NBASIS+1), dtype=np.complex64)
 RANDOM_STATE = ID
-param_gen = potential_gen(NBASIS, LOW_NUM_Q, HIGH_NUM_Q, LOW_V0, HIGH_V0, NE, RANDOM_STATE)
+param_gen = potential_gen(NBASIS, NQ, LOW_V0, HIGH_V0, NE, RANDOM_STATE)
 for i in range(NSAMPLE_PER_PROC):
     hamilton_mat, Vq = next(param_gen)
     T, mu, density = solver(NK, NBASIS, NE, hamilton_mat)
     DATA_STORAGE[i] = np.array([T, *density], dtype=np.complex64)
-    POTENTIAL_STORAGE[i] = np.array([mu, *Vq], dtype=np.complex64)
+    Vq[0] += -mu
+    POTENTIAL_STORAGE[i] = Vq
 
 DATA = None
 POTENTIAL = None
 if ID == 0:
     DATA = np.zeros((NSAMPLES, NBASIS+1), dtype=np.complex64)
-    POTENTIAL = np.zeros((NSAMPLES, NBASIS+1), dtype=np.complex64)
+    POTENTIAL = np.zeros((NSAMPLES, NBASIS), dtype=np.complex64)
 
 comm.Gather(DATA_STORAGE, DATA, root=0)
 comm.Gather(POTENTIAL_STORAGE, POTENTIAL, root=0)
--- tools/ml_fit.py
+++ tools/ml_fit.py
@@ -1,57 +1,85 @@
 import pickle
-import numpy as np 
-from statslib.main.kernel_ridge import KernelRidge
+import numpy as np
 from statslib.tools.utils import rbfKernel, laplaceKernel
 
-with open('data_file/quantum', 'rb') as f:
+np.random.seed(23)
+
+with open('/home/hongbin/Documents/project/MLEK/data_file/quantum', 'rb') as f:
     data = pickle.load(f)
 
-with open('data_file/potential', 'rb') as f1:
+with open('/home/hongbin/Documents/project/MLEK/data_file/potential', 'rb') as f1:
     potential = pickle.load(f1)
 
 N = data.shape[0]
 index = np.arange(0, N, 1, dtype='int')
 np.random.shuffle(index)
 
-train_X, train_y = data[index[:200], 1:], data[index[:200], 0].real
-test_X, test_y = data[index[300:], 1:], data[index[300:], 0].real
+train_X, train_y, train_dy = data[index[:40], 1:], data[index[:40], 0].real, potential[index[:40]]
+test_X, test_y, test_dy = data[index[50:], 1:], data[index[50:], 0].real, potential[index[50:]]
 mean = np.mean(train_y, axis=0)
 train_y -= mean
 test_y -= mean
 
-gamma = 1
+from statslib.tools.multitask_KRR import Multi_task_KRR
+from statslib.main.kernel_ridge import KernelRidge
+from MLEK.main.utils import irfft
+from statslib.tools.utils import laplaceKernel_gd, rbfKernel_gd
+gamma = 5
+# kernel = rbfKernel(gamma)
+# kernel_gd = rbfKernel_gd(gamma)
 kernel = laplaceKernel(gamma)
-model = KernelRidge(kernel, 1e-3)
-model.fit(train_X, train_y[:, np.newaxis])
+kernel_gd = laplaceKernel_gd(gamma)
+model = Multi_task_KRR(kernel, kernel_gd, 1e-5)
+model.fit(train_X, train_y[:, np.newaxis], train_dy)
 predict_y = model.predict(test_X)
 
+model1 = KernelRidge(kernel, 1e-5)
+model1.fit(train_X, train_y[:, np.newaxis])
+predict_y1 = model1.predict(test_X)
+
 err = np.mean((predict_y-test_y)**2)
+err1 = np.mean((predict_y1-test_y)**2)
 print(err)
+print(err1)
 
-alpha = model.coef_
-Xi = model.X_fit_
-def gradient(dens_q):
-    D = dens_q[np.newaxis, :] - Xi
-    manhattan = np.sqrt(D*D.conj())
-    manhattan[:, 0] += 1e-8
-    Kvector = np.squeeze(kernel(dens_q[np.newaxis, :], Xi))
-    mat = alpha[:, np.newaxis] * D.conj() * Kvector[:, np.newaxis] / manhattan
-    return -gamma*np.sum(mat, axis=0)
-from MLEK.main.utils import irfft
-test_i = 23
+def gradient(dens_q, model):
+    inner_dens_q = dens_q[np.newaxis, :]
+    # KM = kernel(inner_dens_q, train_X)
+    alpha = model.coef_
+    KM_gd = kernel_gd(inner_dens_q, train_X)
+    # D_mat = inner_dens_q - train_X
+    # D_div = np.sqrt((D_mat*D_mat.conj()).real)
+    # np.maximum(1e-15, D_div, out=D_div)
+    # gd_q = -gamma*np.sum(alpha[:, np.newaxis]*(D_mat/D_div)*KM.T, axis=0, dtype=np.complex64)
+    # gd_q = -2*gamma*np.sum(alpha[:, np.newaxis] * (inner_dens_q - train_X) * KM.T, axis=0, dtype=np.complex64)
+    gd_q = KM_gd @ alpha
+    return np.squeeze(gd_q)
 
-dTq = gradient(test_X[test_i])
-dTx = irfft(dTq, 100)
+# from statslib.tools.multitask import Multi_task_Regressor
+# from statslib.tools.multitask import Special_SGD
+# optimizer = Special_SGD(1e-5, 1e-3, 1000, n_batch=200, verbose=1)
+# gamma = 1
+# kernel = laplaceKernel(gamma)
+# model = Multi_task_Regressor(kernel, 1e-3, optimizer)
+# model.fit(train_X, train_y[:, np.newaxis], train_dy, gamma)
+# predict_y = model.predict(test_X)
+# err = np.mean((predict_y - test_y)**2)
+# print(err)
+
+import matplotlib.pyplot as plt
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+ax1.plot(test_y, predict_y, 'bo')
+ax1.plot(test_y, predict_y1, 'go', alpha=0.8)
+ax1.plot(test_y, test_y, 'r')
+
+predict_gd_q = gradient(test_X[31], model)
+predict_gd_q1 = gradient(test_X[31], model1)
+predict_gd_x = irfft(predict_gd_q, 100)
+predict_gd_x1 = irfft(predict_gd_q1, 100)
 X = np.linspace(0, 1, 100)
-
-test_potential = potential[index[300:], 1:]
-test_mu = test_potential[test_i, 0]
-test_Vq = test_potential[test_i]
-test_Vx = irfft(test_Vq, 100)
-
-import matplotlib.pyplot as plt 
-plt.plot(X, dTx, 'b')
-plt.plot(X, test_mu - test_Vx, 'r')
-# plt.plot(test_y, predict_y, 'bo')
-# plt.plot(test_y, test_y, 'r')
+true_gd_q = test_dy[31]
+true_gd_x = irfft(true_gd_q, 100)
+ax2.plot(X, -true_gd_x, 'r')
+ax2.plot(X, predict_gd_x, 'b')
+ax2.plot(X, predict_gd_x1, 'g', alpha=0.8)
 plt.show()
