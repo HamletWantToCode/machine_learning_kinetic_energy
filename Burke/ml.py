6efc215d798f8eb32f6ba29110c164aacf318182
hongbinren <hongbinrenscu@outlook.com>
2018-12-02 14:53:37 +0800

    update

--- Burke/ml.py
+++ Burke/ml.py
@@ -3,9 +3,9 @@ import numpy as np
 from statslib.main.kernel_ridge import KernelRidge
 from statslib.tools.utils import rbfKernel, meanSquareError, max_abs_error, mean_abs_error
 
-np.random.seed(11)
+np.random.seed(8)
 
-with open('/media/hongbin/Elements/project/Burke_paper/quantumX1D', 'rb') as f:
+with open('/Users/hongbinren/Downloads/mnt/project/Burke_paper/quantumX1D', 'rb') as f:
     data = pickle.load(f)
 np.random.shuffle(data)
 train_data = data[:1001]
@@ -15,13 +15,16 @@ test_data = data[1001:]
 train_n = train_data[train_data[:, 0]==1]
 test_n = test_data[test_data[:, 0]==1]
 train_X, train_y = train_n[:100, 2:], train_n[:100, 1]
+mean_X = np.mean(train_X, axis=0, keepdims=True)
+train_X -= mean_X
 mean_KE = np.mean(train_y)
-train_y /= mean_KE
+train_y -= mean_KE
 test_X, test_y = test_n[:, 2:], test_n[:, 1]
-test_y /= mean_KE
+test_X -= mean_X
+test_y -= mean_KE
 
-gamma = 1.0/(2*20**2)
-Lambda = 12*1e-14
+gamma = 1.0/(2*18.4207**2)
+Lambda = 3.79269*1e-9
 kernel = rbfKernel(gamma)
 model = KernelRidge(kernel, Lambda)
 model.fit(train_X, train_y[:, np.newaxis])
