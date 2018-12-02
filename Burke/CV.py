80008d10f79e07538eb0edd55a88759cb4ae0233
hongbin <hongbinrenscu@outlook.com>
2018-12-02 14:55:06 +0800

    small changes

--- Burke/CV.py
+++ Burke/CV.py
@@ -7,7 +7,7 @@ from statslib.main.cross_validation import Cross_validation
 
 np.random.seed(8)
 
-with open('/Users/hongbinren/Downloads/mnt/project/Burke_paper/quantumX1D', 'rb') as f:
+with open('/media/hongbin/Elements/project/Burke_paper/quantumX1D', 'rb') as f:
     data = pickle.load(f)
 np.random.shuffle(data)
 train_data = data[:1001]
@@ -17,14 +17,17 @@ test_data = data[1001:]
 train_n = train_data[train_data[:, 0]==1]
 test_n = test_data[test_data[:, 0]==1]
 train_X, train_y = train_n[:100, 2:], train_n[:100, 1]
+mean_X = np.mean(train_X, axis=0, keepdims=True)
+train_X -= mean_X
 mean_KE = np.mean(train_y)
-train_y /= mean_KE
+train_y -= mean_KE
 test_X, test_y = test_n[:, 2:], test_n[:, 1]
-test_y /= mean_KE
+test_X -= mean_X
+test_y -= mean_KE
 
 # plot coef contour
-n_s, n_l = 20, 10
-Sigma = np.logspace(-2, 4, n_s)
+n_s, n_l = 50, 20
+Sigma = np.logspace(-2, 3, n_s)
 Lambda = np.logspace(-16, -4, n_l)
 xx, yy = np.meshgrid(Sigma, Lambda)
 XY = np.c_[xx.reshape((-1, 1)), yy.reshape((-1, 1))]
